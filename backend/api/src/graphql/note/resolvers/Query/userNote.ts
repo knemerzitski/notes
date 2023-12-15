d95dfa41b88a70dd0e574a97182901aa43616671
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-12-15 18:51:09 +0200

    feat: userNote resolver

--- backend/api/src/graphql/note/resolvers/Query/userNote.ts
+++ backend/api/src/graphql/note/resolvers/Query/userNote.ts
@@ -1,8 +1,104 @@
+import { GraphQLError } from 'graphql';
+import { Require_id, Types } from 'mongoose';
+
 import type { QueryResolvers } from '../../../../graphql/types.generated';
-export const userNote: NonNullable<QueryResolvers['userNote']> = () =>
-  // _parent,
-  // _arg,
-  // _ctx
-  {
-    throw new Error('Not implemented');
+import { DBNote } from '../../../../mongoose/models/note';
+import { DBUserNote } from '../../../../mongoose/models/user-note';
+import { assertAuthenticated } from '../../../base/directives/auth';
+
+type UserNoteWithoutIds = Omit<DBUserNote, 'userId' | 'noteId'> & {
+  _id?: Types.ObjectId;
+};
+type UserNoteWithNote = UserNoteWithoutIds & { note: Require_id<DBNote> };
+
+type AggregateResult = UserNoteWithNote;
+
+export const userNote: NonNullable<QueryResolvers['userNote']> = async (
+  _parent,
+  { id: notePublicId },
+  ctx
+) => {
+  const {
+    auth,
+    mongoose: { model },
+  } = ctx;
+  assertAuthenticated(auth);
+
+  const results = await model.Note.aggregate<AggregateResult>([
+    {
+      $match: {
+        publicId: notePublicId,
+      },
+    },
+    {
+      $project: { note: '$$ROOT' },
+    },
+    {
+      $lookup: {
+        // all UserNotes that have notePublicId
+        from: model.UserNote.collection.collectionName,
+        foreignField: 'noteId',
+        localField: '_id',
+        as: 'userNote',
+
+        pipeline: [
+          {
+            // Ensure current user has access to notePublicId
+            $match: {
+              userId: auth.session.user._id._id,
+            },
+          },
+          { $unset: ['userId', 'noteId'] },
+        ],
+      },
+    },
+    {
+      $set: {
+        userNote: { $arrayElemAt: ['$userNote', 0] },
+      },
+    },
+    {
+      $addFields: {
+        'userNote.note': '$note',
+      },
+    },
+    {
+      $replaceRoot: {
+        newRoot: '$userNote',
+      },
+    },
+  ]);
+
+  const userNote = results[0];
+
+  if (!userNote) {
+    throw new GraphQLError('Note not found', {
+      extensions: {
+        code: 'NOT_FOUND',
+      },
+    });
+  }
+
+  if (!userNote._id) {
+    throw new GraphQLError('You are not authorized to access this note.', {
+      extensions: {
+        code: 'UNAUTHORIZED',
+      },
+    });
+  }
+
+  const { note } = userNote;
+
+  return {
+    id: note.publicId,
+    readOnly: userNote.readOnly,
+    preferences: {
+      backgroundColor: userNote.preferences?.backgroundColor,
+    },
+    note: {
+      id: note.publicId,
+      title: note.title ?? '',
+      textContent: note.textContent ?? '',
+    },
   };
+};
