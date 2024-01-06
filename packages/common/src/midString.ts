ba23c7808b5a46edde37fdba443fa82124315db4
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-01-06 18:59:03 +0200

    feat: midString dynamic range

--- packages/common/src/midString.ts
+++ packages/common/src/midString.ts
@@ -1,47 +1,45 @@
-const CHAR_A = 'a';
-const CHAR_Z = 'z';
-
-const CODE_START = 96;
-const CODE_A = 97;
-const CODE_B = 98;
-const CODE_Z = 122;
-const CODE_END = 123;
-
 /**
  * @see {@link https://stackoverflow.com/questions/38923376/return-a-new-string-that-sorts-between-two-given-strings/38927158#38927158}
  * On empty strings returns "n"
+ * @param {number} firstCode First character code to include in lookup, defaults to 97 = "a"
+ * @param {number} lastCode Last character code to include in lookup, defaults to 122 = "z"
  * @returns smallest possible string that is lexicographically ordered between {@link prev} and {@link next}
  */
-export default function midString(prev: string, next: string): string {
+export default function midString(prev: string, next: string, firstCode = 97, lastCode = 122): string {
+  const codeStart = firstCode - 1;
+  const codeEnd = lastCode + 1;
+  const charFirst = String.fromCharCode(firstCode);
+  const charLast = String.fromCharCode(lastCode);
+
   // Find leftmost non-matching position
   let pos = 0;
   let codePrev = 0;
   let codeNext = 0;
   for (; codePrev === codeNext; pos++) {
-    codePrev = pos < prev.length ? prev.charCodeAt(pos) : CODE_START;
-    codeNext = pos < next.length ? next.charCodeAt(pos) : CODE_END;
+    codePrev = pos < prev.length ? prev.charCodeAt(pos) : codeStart;
+    codeNext = pos < next.length ? next.charCodeAt(pos) : codeEnd;
   }
 
   // Matching left part of string
   let str = prev.slice(0, pos - 1);
 
-  if (codePrev === CODE_START) {
-    while (codeNext === CODE_A) {
-      codeNext = pos < next.length ? next.charCodeAt(pos++) : CODE_END;
-      str += CHAR_A;
+  if (codePrev === codeStart) {
+    while (codeNext === firstCode) {
+      codeNext = pos < next.length ? next.charCodeAt(pos++) : codeEnd;
+      str += charFirst;
     }
-    if (codeNext === CODE_B) {
-      str += CHAR_A;
-      codeNext = CODE_END;
+    if (codeNext === firstCode + 1) {
+      str += charFirst;
+      codeNext = codeEnd;
     }
   } else if (codePrev + 1 === codeNext) {
     // Two consecutive chars
     str += String.fromCharCode(codePrev);
-    codeNext = CODE_END;
+    codeNext = codeEnd;
     while (
-      (codePrev = pos < prev.length ? prev.charCodeAt(pos++) : CODE_START) == CODE_Z
+      (codePrev = pos < prev.length ? prev.charCodeAt(pos++) : codeStart) == lastCode 
     ) {
-      str += CHAR_Z;
+      str += charLast;
     }
   }
 
