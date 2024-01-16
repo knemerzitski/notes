c1973a5b27bad213853fcbb0637518e95e794741
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-01-16 18:56:57 +0200

    refactor: delete unused file

--- packages/app/src/utils/transformDocument.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentTransform, TypedDocumentNode } from '@apollo/client';
-
-export default function transformDocument<TResult, TVariables>(
-  documentTransform: DocumentTransform,
-  document: TypedDocumentNode<TResult, TVariables>
-): TypedDocumentNode<TResult, TVariables> {
-  return documentTransform.transformDocument(document);
-}
