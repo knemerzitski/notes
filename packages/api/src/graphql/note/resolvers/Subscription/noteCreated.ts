78300a7ace2b7ab469463929393b38db50da823c
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-04-21 17:23:15 +0300

    refactor: subscription topics in enum

--- packages/api/src/graphql/note/resolvers/Subscription/noteCreated.ts
+++ packages/api/src/graphql/note/resolvers/Subscription/noteCreated.ts
@@ -1,29 +1,28 @@
 import { GraphQLResolversContext } from '../../../context';
-import type {
-  NoteCreatedPayload,
-  SubscriptionResolvers,
-} from '../../../types.generated';
+import type { ResolversTypes, SubscriptionResolvers } from '../../../types.generated';
 import { isAuthenticated } from '../../../auth-context';
 import { assertAuthenticated } from '../../../base/directives/auth';
+import { SubscriptionTopicPrefix } from '../../../subscriptions';
 
 export const noteCreated: NonNullable<SubscriptionResolvers['noteCreated']> = {
   subscribe: (_parent, _arg, { auth, subscribe, denySubscription }) => {
     if (!isAuthenticated(auth)) return denySubscription();
 
-    return subscribe(`NOTE_CREATED:user-${auth.session.user.publicId}`);
-  },
-  resolve(payload: NoteCreatedPayload) {
-    return payload;
+    const userId = auth.session.user._id.toString('base64');
+
+    return subscribe(`${SubscriptionTopicPrefix.NoteCreated}:${userId}`);
   },
 };
 
 export async function publishNoteCreated(
   { publish, auth }: GraphQLResolversContext,
-  payload: NoteCreatedPayload
+  payload: ResolversTypes['NoteCreatedPayload']
 ) {
   assertAuthenticated(auth);
 
-  return publish(`NOTE_CREATED:user-${auth.session.user.publicId}`, {
+  const userId = auth.session.user._id.toString('base64');
+
+  return publish(`${SubscriptionTopicPrefix.NoteCreated}:${userId}`, {
     noteCreated: payload,
   });
 }
---
+++ packages/api/src/graphql/subscriptions.ts
@@ -0,0 +1,5 @@
+export enum SubscriptionTopicPrefix {
+  NoteCreated = 'NOTE_CREATED',
+  NoteUpdated = 'NOTE_UPDATED',
+  NoteDeleted = 'NOTE_DELETED',
+}
