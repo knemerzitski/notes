4daa033cb160c4f112991452d14fa5135e178544
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-08-23 11:50:44 +0300

    feat(api): syncSessionCookies with tests

---
+++ packages/api/src/graphql/auth/resolvers/Mutation/syncSessionCookies.int.test.ts
@@ -0,0 +1,130 @@
+import { expect, it } from 'vitest';
+import { apolloServer } from '../../../../__test__/helpers/graphql/apollo-server';
+import {
+  CreateGraphQLResolversContextOptions,
+  createGraphQLResolversContext,
+} from '../../../../__test__/helpers/graphql/graphql-context';
+import {
+  SyncSessionCookiesInput,
+  SyncSessionCookiesPayload,
+} from '../../../types.generated';
+import { Cookies } from '../../../../services/http/cookies';
+import { ObjectId } from 'mongodb';
+import { sessionDefaultValues } from '../../../../mongodb/schema/session/session';
+import { objectIdToStr } from '../../../../services/utils/objectid';
+import { expectGraphQLResponseData } from '../../../../__test__/helpers/graphql/response';
+
+interface Variables {
+  input: SyncSessionCookiesInput;
+}
+
+const MUTATION = `#graphql
+  mutation SignOut($input: SyncSessionCookiesInput!) {
+    syncSessionCookies(input: $input) {
+      availableUserIds
+    }
+  }
+`;
+
+async function executeOperation(
+  input: Variables['input'],
+  options?: CreateGraphQLResolversContextOptions,
+  query: string = MUTATION
+) {
+  return await apolloServer.executeOperation<
+    {
+      syncSessionCookies: SyncSessionCookiesPayload;
+    },
+    Variables
+  >(
+    {
+      query,
+      variables: {
+        input,
+      },
+    },
+    {
+      contextValue: createGraphQLResolversContext(options),
+    }
+  );
+}
+
+it('removes userIds in cookies not known to client', async () => {
+  const userId = new ObjectId();
+  const cookieId = sessionDefaultValues.cookieId();
+
+  const cookies = new Cookies();
+  cookies.setSession(userId, cookieId);
+  cookies.setSession(new ObjectId(), sessionDefaultValues.cookieId());
+
+  const multiValueHeaders: Record<string, (string | number | boolean)[]> = {};
+
+  const response = await executeOperation(
+    {
+      availableUserIds: [objectIdToStr(userId)],
+    },
+    {
+      override: {
+        cookies,
+        response: {
+          multiValueHeaders,
+        },
+      },
+    }
+  );
+
+  const data = expectGraphQLResponseData(response);
+  expect(data).toEqual({
+    syncSessionCookies: {
+      availableUserIds: [objectIdToStr(userId)],
+    },
+  });
+
+  expect(cookies.getAvailableSessionUserIds()).toStrictEqual([objectIdToStr(userId)]);
+
+  expect(multiValueHeaders).toEqual({
+    'Set-Cookie': [
+      `Sessions=${objectIdToStr(userId)}:${cookieId}; HttpOnly; SameSite=Strict; Path=/`,
+    ],
+  });
+});
+
+it('ignores unknown client userIds not in cookies', async () => {
+  const userId = new ObjectId();
+  const unknownUserId = new ObjectId();
+  const cookieId = sessionDefaultValues.cookieId();
+
+  const cookies = new Cookies();
+  cookies.setSession(userId, cookieId);
+
+  const multiValueHeaders: Record<string, (string | number | boolean)[]> = {};
+
+  const response = await executeOperation(
+    {
+      availableUserIds: [objectIdToStr(userId), objectIdToStr(unknownUserId)],
+    },
+    {
+      override: {
+        cookies,
+        response: {
+          multiValueHeaders,
+        },
+      },
+    }
+  );
+
+  const data = expectGraphQLResponseData(response);
+  expect(data).toEqual({
+    syncSessionCookies: {
+      availableUserIds: [objectIdToStr(userId)],
+    },
+  });
+
+  expect(cookies.getAvailableSessionUserIds()).toStrictEqual([objectIdToStr(userId)]);
+
+  expect(multiValueHeaders).toEqual({
+    'Set-Cookie': [
+      `Sessions=${objectIdToStr(userId)}:${cookieId}; HttpOnly; SameSite=Strict; Path=/`,
+    ],
+  });
+});
--- packages/api/src/graphql/auth/resolvers/Mutation/syncSessionCookies.ts
+++ packages/api/src/graphql/auth/resolvers/Mutation/syncSessionCookies.ts
@@ -1,6 +1,20 @@
 import type { MutationResolvers } from './../../../types.generated';
-export const syncSessionCookies: NonNullable<
-  MutationResolvers['syncSessionCookies']
-> = async (_parent, _arg, _ctx) => {
-  /* Implement Mutation.syncSessionCookies resolver logic here */
+
+export const syncSessionCookies: NonNullable<MutationResolvers['syncSessionCookies']> = (
+  _parent,
+  arg,
+  ctx
+) => {
+  const { response, cookies } = ctx;
+  const {
+    input: { availableUserIds },
+  } = arg;
+
+  cookies.filterSessionsByUserId(availableUserIds);
+
+  cookies.putCookiesToHeaders(response.multiValueHeaders);
+
+  return {
+    availableUserIds: cookies.getAvailableSessionUserIds(),
+  };
 };
