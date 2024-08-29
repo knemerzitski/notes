ae66cdad2460117c84d148546055797f61cabc75
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-08-29 20:03:33 +0300

    chore(api): graphql generated

---
+++ packages/api/src/graphql/domains/note/resolvers/Mutation/deleteNote.ts
@@ -0,0 +1,8 @@
+import type { MutationResolvers } from './../../../types.generated';
+export const deleteNote: NonNullable<MutationResolvers['deleteNote']> = async (
+  _parent,
+  _arg,
+  _ctx
+) => {
+  /* Implement Mutation.deleteNote resolver logic here */
+};
