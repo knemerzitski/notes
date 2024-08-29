0b4eff49626e1adaf547ad3810fd263cbb15e63a
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-08-29 20:16:50 +0300

    feat(utils): isArray separate for readonly

--- packages/utils/src/array/is-array.ts
+++ packages/utils/src/array/is-array.ts
@@ -1,6 +1,7 @@
 // TODO move to type-guards
 export function isArray<T, TItem>(
   maybeArr: T | readonly TItem[]
-): maybeArr is readonly TItem[] {
+): maybeArr is readonly TItem[];
+export function isArray<T, TItem>(maybeArr: T | TItem[]): maybeArr is TItem[] {
   return Array.isArray(maybeArr);
 }
