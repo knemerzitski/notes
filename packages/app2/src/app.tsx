5346cd98224002197e5669eba6791c69eec23077
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-10-02 22:00:26 +0300

    refactor(app2): rename theme-provider to theme

--- packages/app2/src/app.tsx
+++ packages/app2/src/app.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline } from '@mui/material';
 
 import { GlobalStyles } from './global-styles';
-import { CustomThemeProvider } from './theme/context/theme-provider';
+import { CustomThemeProvider } from './theme/context/theme';
 import { createThemeOptions } from './theme-options';
 
 export function App() {
