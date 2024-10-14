f23c3a3a248e3bdeba08461cc5098e4f851da1b7
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-10-14 20:01:36 +0300

    refactor(app2): use import.meta.env instead

--- packages/app2/src/routes/components/TanStackRouterDevTools.ts
+++ packages/app2/src/routes/components/TanStackRouterDevTools.ts
@@ -3,12 +3,11 @@ import React from 'react';
 /**
  * @source {@link https://tanstack.com/router/v1/docs/framework/react/devtools}
  */
-export const TanStackRouterDevtools =
-  process.env.NODE_ENV === 'production'
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-        // Lazy load in development
-        import('@tanstack/router-devtools').then((res) => ({
-          default: res.TanStackRouterDevtools,
-        }))
-      );
+export const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+      }))
+    );
