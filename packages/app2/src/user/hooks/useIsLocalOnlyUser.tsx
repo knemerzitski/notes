0c0c377954f20426f0a79c5c171d0a69fb35cba4
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-11-26 21:44:52 +0200

    feat(app2): useIsLocalOnlyUser

---
+++ packages/app2/src/user/hooks/useIsLocalOnlyUser.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-restricted-imports
+import { useQuery } from '@apollo/client';
+import { gql } from '../../__generated__';
+import { useUserId } from '../context/user-id';
+
+const UseIsLocalOnlyUser_Query = gql(`
+  query UseIsLocalOnlyUser_Query($id: ID!) {
+    signedInUser(by: { id: $id }) @client {
+      id
+      localOnly
+    }
+  }
+`);
+
+export function useIsLocalOnlyUser() {
+  const userId = useUserId();
+  const { data } = useQuery(UseIsLocalOnlyUser_Query, {
+    variables: {
+      id: userId,
+    },
+    fetchPolicy: 'cache-only',
+  });
+
+  return data?.signedInUser?.localOnly ?? false;
+}
