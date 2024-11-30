b30b0ce833447da2693d3397afaf841dc17c7e4b
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-11-30 14:32:20 +0200

    fix(app): remove unused route file

--- packages/app/src/routes/note-index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_root_layout/note/')({
-  component: NoteIndex,
-})
-
-function NoteIndex() {
-  return 'Note Index'
-}
