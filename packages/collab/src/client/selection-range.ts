04f078d1b17baf78bec81cf4dc58ef40d11a7266
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-12-17 20:15:05 +0200

    feat(collab): allow end to be null

--- packages/collab/src/client/selection-range.ts
+++ packages/collab/src/client/selection-range.ts
@@ -39,13 +39,21 @@ export namespace SelectionRange {
     return { start: a.start + b.start, end: a.end + b.end };
   }
 
-  export function from(start?: number, end?: number): SelectionRange;
+  export function from(start?: number, end?: Maybe<number>): SelectionRange;
   export function from(
-    selection?: Readonly<PartialBy<SelectionRange, 'end'>>
+    selection?: Readonly<{
+      start: number;
+      end?: Maybe<number>;
+    }>
   ): SelectionRange;
   export function from(
-    selection: Readonly<PartialBy<SelectionRange, 'end'>> | number = 0,
-    end?: number
+    selection:
+      | Readonly<{
+          start: number;
+          end?: Maybe<number>;
+        }>
+      | number = 0,
+    end?: Maybe<number>
   ): SelectionRange {
     if (typeof selection === 'number') {
       return { start: selection, end: end ?? selection };
