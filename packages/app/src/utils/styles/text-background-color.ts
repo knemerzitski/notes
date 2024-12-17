235c7d13dc88932e173a302503ec625f4d68c937
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-12-17 20:03:40 +0200

    feat(app): export stringToColor

--- packages/app/src/utils/styles/text-background-color.ts
+++ packages/app/src/utils/styles/text-background-color.ts
@@ -1,25 +1,5 @@
 import { css } from '@mui/material';
-
-/**
- * @see {@link https://mui.com/material-ui/react-avatar/}
- */
-function stringToColor(string: string) {
-  let hash = 0;
-  let i;
-
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-
-  return color;
-}
+import { stringToColor } from '../string-to-color';
 
 export const textBackgroundColor = {
   style: ({ bgColorText }: { bgColorText: string }) => {
