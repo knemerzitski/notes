f8ebdd1dd061b069127712d924f4857dc1b56aee
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-01-24 19:15:32 +0200

    feat(lambda-graphql): BaseGraphQLContextTransformer object instead of unknown

--- packages/lambda-graphql/src/types.ts
+++ packages/lambda-graphql/src/types.ts
@@ -1,8 +1,8 @@
 import { Maybe } from '~utils/types';
 
 export interface BaseGraphQLContextTransformer<T> {
-  serialize: (value: Maybe<T>) => unknown;
-  parse: (value: Maybe<unknown>) => T;
+  serialize: (value: Maybe<T>) => object;
+  parse: (value: Maybe<object>) => T;
 }
 
 export interface CommonWebSocketDirectParams<TPersistGraphQLContext> {
