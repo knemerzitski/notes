215ce716c5ca0dda42600c9036a36d6b8542628e
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-01-28 17:08:47 +0200

    feat(app): signedInUser.noteLinkConnection policy

--- packages/app/src/note/policies/SignedInUser.ts
+++ packages/app/src/note/policies/SignedInUser.ts
@@ -2,6 +2,8 @@ import { isObjectLike } from '~utils/type-guards/is-object-like';
 import { CreateTypePolicyFn, TypePoliciesContext } from '../../graphql/types';
 import { throwNoteNotFoundError } from '../utils/errors';
 import { getUserNoteLinkId } from '../utils/id';
+import { isReference } from '@apollo/client';
+import { relayStylePagination } from '../../graphql/utils/relay-style-pagination';
 
 export const SignedInUser: CreateTypePolicyFn = function (_ctx: TypePoliciesContext) {
   return {
@@ -59,6 +61,62 @@ export const SignedInUser: CreateTypePolicyFn = function (_ctx: TypePoliciesCont
         },
         merge: false,
       },
+      noteLinkConnection: relayStylePagination(['category'], {
+        read(
+          existing = {
+            __typename: 'UserNoteLinkConnection',
+            edges: [],
+            pageInfo: {
+              hasPreviousPage: false,
+              hasNextPage: false,
+              startCursor: null,
+              endCursor: null,
+            },
+          }
+        ) {
+          return existing;
+        },
+        // Preserve local edges (when merging incoming from server) by checking node.note.localOnly
+        preserveEdge(edge, { readField }) {
+          const node = readField('node', edge);
+          if (!isReference(node)) {
+            return false;
+          }
+
+          const note = readField('note', node);
+          if (!isReference(note)) {
+            return false;
+          }
+
+          return !!readField('localOnly', note);
+        },
+        /**
+         * Currently userNoteLinkConnection cursors are derived from Note.id
+         * and can be read from edge.
+         */
+        getCursor(edge, { readField }) {
+          if (!edge) {
+            return;
+          }
+
+          const node = readField('node', edge);
+          if (!isReference(node)) {
+            return;
+          }
+
+          const note = readField('note', node);
+          if (!isReference(note)) {
+            return;
+          }
+
+          const noteId = readField('id', note);
+          if (typeof noteId === 'string') {
+            return noteId;
+          }
+          return;
+        },
+        isOrderedSet: true,
+      }),
     },
   };
 };
