ec9728c570303ef49fc665c3af9038c4c15a77db
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-01-29 17:38:38 +0200

    feat: comment out auth directive

--- packages/api/src/graphql/domains/base/directives/auth.ts
+++ packages/api/src/graphql/domains/base/directives/auth.ts
@@ -1,15 +1,32 @@
+// TODO @auth directive is not used but remains here as documentation
 import { GraphQLSchema } from 'graphql/index.js';
 
 import { transformSchemaDirectiveResolver } from '../../../utils/transform-schema-directive-resolver';
 import type { DirectiveResolvers } from '../../types.generated';
 
+
+/*
+"""
+Requires user to be authenticated and with access to specific role
+"""
+directive @auth(requires: Role = USER) on OBJECT | FIELD_DEFINITION
+
+"""
+TODO desc
+"""
+enum Role {
+  USER
+}
+*/
+
 export const auth: NonNullable<DirectiveResolvers['auth']> = async (
   next,
   _parent,
   _args,
-  ctx
+  _ctx
 ) => {
-  await ctx.services.requestHeaderAuth.getAuth();
+  // TODO uncomment to enable auth
+  // await ctx.services.requestHeaderAuth.getAuth();
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return next();
