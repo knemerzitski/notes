25d964c3f828f749ad1724ee4fccfb14350a223e
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-01-29 19:52:10 +0200

    feat(api): signOut with oneOf directive

--- packages/api/src/graphql/domains/auth/resolvers/Mutation/signOut.ts
+++ packages/api/src/graphql/domains/auth/resolvers/Mutation/signOut.ts
@@ -14,7 +14,7 @@ export const signOut: NonNullable<MutationResolvers['signOut']> = async (
   const { input } = arg;
 
   let signedOutUserIds: string[] = [];
-  if (input?.allUsers) {
+  if (input.allUsers) {
     // Sign out all users
     signedOutUserIds = services.auth
       .getAvailableUserIds()
@@ -22,20 +22,11 @@ export const signOut: NonNullable<MutationResolvers['signOut']> = async (
       .filter(isDefined);
 
     await services.auth.deleteAllAuth();
-  } else {
-    if (input?.userId) {
-      // Sign out specified user
-      signedOutUserIds = [objectIdToStr(input.userId)];
+  } else if (input.user) {
+    // Sign out specified user
+    signedOutUserIds = [objectIdToStr(input.user.id)];
 
-      await services.auth.deleteAuthByUserId(input.userId);
-    } else if (await ctx.services.requestHeaderAuth.isAuthenticated()) {
-      // Sign out authenticated user
-      const auth = await ctx.services.requestHeaderAuth.getAuth();
-
-      signedOutUserIds = [objectIdToStr(auth.session.userId)];
-
-      await ctx.services.auth.deleteAuthByUserId(auth.session.userId);
-    }
+    await services.auth.deleteAuthByUserId(input.user.id);
   }
 
   return {
