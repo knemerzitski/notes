2279828a5761f609acdf06db4362f9217fcfe90c
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-02-05 22:22:33 +0200

    feat(app): pwa setup and logo generation

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { virtualRouteConfig } from './virtual-routes.config';
 
@@ -21,6 +22,49 @@ export default ({ mode }: { mode: string }) => {
         generatedRouteTree: 'src/__generated__/routeTree.gen.ts',
       }),
       tsconfigPaths(),
+      /**
+       * @see {@link https://vite-pwa-org.netlify.app/guide/}
+       */
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'inline',
+        manifest: {
+          name: 'Notes',
+          short_name: 'Notes',
+          description: 'Collaborative note taking app',
+          start_url: '/',
+          theme_color: '#92fde1',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+        devOptions: {
+          enabled: true,
+          resolveTempFolder: () => './dev-out',
+        },
+      }),
     ],
     build: {
       outDir: 'out',
