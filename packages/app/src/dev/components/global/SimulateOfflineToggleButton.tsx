f23b5e7844035f69eac80af419ffc06a0f7b09b3
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-02-06 22:25:38 +0200

    fix(app): dev simulate offline button

--- packages/app/src/dev/components/global/SimulateOfflineToggleButton.tsx
+++ packages/app/src/dev/components/global/SimulateOfflineToggleButton.tsx
@@ -6,12 +6,58 @@ import { GraphQLServiceAction } from '../../../graphql/types';
 
 const STORAGE_KEY = 'devToolsOnline';
 
+class OnlineOverride {
+  private overrideIsOnline: boolean | null;
+  private eventIsOnline: boolean;
+
+  get isOverride() {
+    return this.overrideIsOnline != null;
+  }
+  constructor() {
+    this.overrideIsOnline = null;
+    this.eventIsOnline = window.navigator.onLine;
+    console.log(Object.getOwnPropertyDescriptor(window.navigator, 'onLine'));
+    if (!Object.getOwnPropertyDescriptor(window.navigator, 'onLine')) {
+      Object.defineProperty(window.navigator, 'onLine', {
+        get: () => this.overrideIsOnline ?? this.eventIsOnline,
+      });
+    }
+    window.addEventListener('online', () => {
+      this.eventIsOnline = true;
+    });
+    window.addEventListener('offline', () => {
+      this.eventIsOnline = false;
+    });
+  }
+
+  setOverrideOnline(online: boolean) {
+    this.overrideIsOnline = online;
+    if (online && !this.eventIsOnline) {
+      window.dispatchEvent(new Event('online'));
+    } else if (!online && this.isOverride) {
+      window.dispatchEvent(new Event('offline'));
+    }
+  }
+
+  clearOverrideOnline() {
+    this.overrideIsOnline = null;
+    if (this.eventIsOnline) {
+      window.dispatchEvent(new Event('online'));
+    } else if (!this.isOverride) {
+      window.dispatchEvent(new Event('offline'));
+    }
+  }
+}
+
+let onlineOverride: OnlineOverride | undefined;
 let offlineController: GateController | undefined;
 
 export const simulateOfflineGraphQLServiceAction: GraphQLServiceAction = (service) => {
   offlineController = service.links.gateLink.create();
+  onlineOverride = new OnlineOverride();
   if (readDevIsOnline() === false) {
     offlineController.close();
+    onlineOverride.setOverrideOnline(true);
   }
 };
 
@@ -28,41 +74,76 @@ function readDevIsOnline() {
   return JSON.parse(value);
 }
 
-// TODO doesn't block subscriptions data
 export function SimulateOfflineToggleButton() {
   const [isOpen, setIsOpen] = useState(offlineController?.isOpen ?? false);
+  const [isOverride, setIsOverride] = useState(onlineOverride?.isOverride ?? false);
 
   if (!offlineController) {
     return null;
   }
 
   function handleOpen() {
-    if (!offlineController) return;
+    if (!offlineController || !onlineOverride) return;
 
     setIsOpen(true);
+    onlineOverride.setOverrideOnline(true);
     offlineController.open();
     writeDevIsOnline(true);
+
+    setIsOverride(onlineOverride.isOverride);
   }
 
   function handleClose() {
-    if (!offlineController) return;
+    if (!offlineController || !onlineOverride) return;
 
     setIsOpen(false);
+    onlineOverride.setOverrideOnline(false);
     offlineController.close();
     writeDevIsOnline(false);
+
+    setIsOverride(onlineOverride.isOverride);
+  }
+
+  function handleClear() {
+    if (!offlineController || !onlineOverride) return;
+
+    onlineOverride.clearOverrideOnline();
+    if (window.navigator.onLine) {
+      setIsOpen(true);
+      offlineController.open();
+      writeDevIsOnline(true);
+    } else {
+      setIsOpen(false);
+      offlineController.close();
+      writeDevIsOnline(false);
+    }
+
+    setIsOverride(onlineOverride.isOverride);
   }
 
   return (
-    <Button
-      onClick={() => {
-        if (isOpen) {
-          handleClose();
-        } else {
-          handleOpen();
-        }
-      }}
-    >
-      {isOpen ? 'online' : 'offline'}
-    </Button>
+    <>
+      <Button
+        onClick={() => {
+          if (isOpen) {
+            handleClose();
+          } else {
+            handleOpen();
+          }
+        }}
+      >
+        {isOpen ? 'online' : 'offline'}
+      </Button>
+      {
+        <Button
+          disabled={!isOverride}
+          onClick={() => {
+            handleClear();
+          }}
+        >
+          Clear
+        </Button>
+      }
+    </>
   );
 }
