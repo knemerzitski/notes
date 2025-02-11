5c290a4e1c5e43a95432762f77fb63d147ada40d
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-02-11 13:20:55 +0200

    fix: lint-staged eslint for packages

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,21 @@
-import path from 'node:path';
-import process from 'node:process';
+import { readdir } from 'node:fs/promises';
 
-const eslintCommand = (filenames) =>
-  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
+const ESLINT_GLOB = '*.{js,jsx,ts,tsx,graphql}';
+
+const packages = (await readdir('packages', { withFileTypes: true }))
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
 
 /** @type {import("lint-staged").Config} */
 export default {
-  '*.{js,jsx,ts,tsx,graphql}': [eslintCommand],
+  // Eslint for any other file not included in packages dir
+  [`!(packages/**/${ESLINT_GLOB})`]: 'eslint --fix --max-warnings 0 --no-warn-ignored',
+  // Eslint for each package
+  ...Object.fromEntries(
+    packages.map((packageName) => [
+      `packages/${packageName}/**/${ESLINT_GLOB}`,
+      `npm run -w ${packageName} eslint:lint-staged`,
+    ])
+  ),
   '**/*': ['prettier --write --ignore-unknown'],
 };
