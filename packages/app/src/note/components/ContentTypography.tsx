4c90b2467ee271fcc0e080c16ae348f64372f079
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-02-12 21:00:10 +0200

    fix(app): note card content fade using gradient instead of clip

--- packages/app/src/note/components/ContentTypography.tsx
+++ packages/app/src/note/components/ContentTypography.tsx
@@ -1,8 +1,9 @@
-import { styled, Typography } from '@mui/material';
+import { css, styled, Theme, Typography } from '@mui/material';
 
 import { gql } from '../../__generated__';
 import { NoteTextFieldName } from '../../__generated__/graphql';
 import { useTextFieldValue } from '../hooks/useTextFieldValue';
+import { mergeShouldForwardProp } from '../../utils/merge-should-forward-prop';
 
 const _ContentTypography_NoteFragment = gql(`
   fragment ContentTypography_NoteFragment on Note {
@@ -10,21 +11,64 @@ const _ContentTypography_NoteFragment = gql(`
   }
 `);
 
-export function ContentTypography() {
-  const value = useTextFieldValue(NoteTextFieldName.CONTENT) ?? '';
-
-  return <TypographyStyled>{value}</TypographyStyled>;
+interface TypographyStyledProps {
+  bottomGradient?: {
+    color: ((theme: Theme) => string) | string;
+    height: number;
+  };
 }
 
-// TODO add gradient fade for last line of text
-const TypographyStyled = styled(Typography)({
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  whiteSpace: 'pre-wrap',
-  display: '-webkit-box',
-  WebkitBoxOrient: 'vertical',
-  WebkitLineClamp: '7',
-  textAlign: 'left',
-  flex: '1 1 100%',
-  userSelect: 'none',
-});
+export function ContentTypography(props: TypographyStyledProps) {
+  const value = useTextFieldValue(NoteTextFieldName.CONTENT) ?? '';
+
+  return (
+    <TypographyStyled bottomGradient={props.bottomGradient}>{value}</TypographyStyled>
+  );
+}
+
+const bottomGradient = {
+  style: ({ bottomGradient, theme }: TypographyStyledProps & { theme: Theme }) => {
+    if (!bottomGradient) {
+      return;
+    }
+
+    const color =
+      typeof bottomGradient.color === 'string'
+        ? bottomGradient.color
+        : bottomGradient.color(theme);
+
+    return css`
+      &::after {
+        content: '';
+
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+
+        height: ${theme.spacing(bottomGradient.height)};
+
+        background: linear-gradient(0deg, ${color}, transparent);
+      }
+    `;
+  },
+  props: ['bottomGradient'],
+};
+
+const TypographyStyled = styled(Typography, {
+  shouldForwardProp: mergeShouldForwardProp(bottomGradient.props),
+})<TypographyStyledProps>(
+  {
+    position: 'relative',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'pre-wrap',
+    display: '-webkit-box',
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: '7',
+    textAlign: 'left',
+    flex: '1 1 100%',
+    userSelect: 'none',
+  },
+  bottomGradient.style
+);
