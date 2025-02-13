d4adc169e63ae63d3c775cc62c8b64c5dec1520e
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-02-13 18:54:42 +0200

    feat(app): selected notes bulk delete forever

---
+++ packages/app/src/note/hooks/useDeleteNoteWithConfirm.ts
@@ -0,0 +1,41 @@
+import { useCallback } from 'react';
+
+import { wrapArray } from '~utils/array/wrap-array';
+
+import { Note } from '../../__generated__/graphql';
+import { useShowConfirm } from '../../utils/context/show-confirm';
+
+import { useDeleteNote } from './useDeleteNote';
+
+export function useDeleteNoteWithConfirm() {
+  const deleteNote = useDeleteNote();
+  const showConfirm = useShowConfirm();
+
+  return useCallback(
+    (noteId: Note['id'] | readonly Note['id'][]) => {
+      console.log(noteId);
+      const noteIds = wrapArray(noteId);
+      if (noteIds.length === 0) {
+        return Promise.resolve(true);
+      }
+
+      return new Promise<boolean>((res) => {
+        showConfirm(
+          noteIds.length > 1 ? 'Delete notes forever?' : 'Delete note forever?',
+          {
+            onSuccess() {
+              noteIds.forEach((noteId) => {
+                void deleteNote({ noteId });
+              });
+              res(true);
+            },
+            onCancel() {
+              res(false);
+            },
+          }
+        );
+      });
+    },
+    [deleteNote, showConfirm]
+  );
+}
