fcdf94656373903b5a5aacfc0e1fb5a85ae10835
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-02-18 16:26:49 +0200

    feat(app): component IsLoading

---
+++ packages/app/src/utils/components/IsLoading.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import { useIsLoading } from '../context/is-loading';
+
+export function IsLoading({ children }: { children: ReactNode }) {
+  const isLoading = useIsLoading();
+
+  if (isLoading) {
+    return children;
+  }
+
+  return null;
+}
