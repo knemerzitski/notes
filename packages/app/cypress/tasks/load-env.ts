cb30e44fad6b65d305af6965d8119d7f7c0bea87
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-03-04 15:25:56 +0200

    refactor(app): restructure cypress tasks

--- packages/app/cypress/tasks/load-env.ts
+++
@@ -1,9 +0,0 @@
-import { loadEnvironmentVariables } from '../../../utils/src/env';
-import { createLogger } from '../../../utils/src/logging';
-import { logNodeInfo } from '../../../utils/src/node';
-
-const logger = createLogger('load-env');
-
-logNodeInfo(logger);
-
-loadEnvironmentVariables(logger);
