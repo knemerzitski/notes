dd87474d2c8291e7d588dd51bdb9c3a83df13e6c
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-03-07 17:18:00 +0200

    refactor(api): createMongoDBApiInstanceContext

--- packages/api/src/__tests__/helpers/mongodb/instance.ts
+++ packages/api/src/__tests__/helpers/mongodb/instance.ts
@@ -2,6 +2,8 @@ import { Collection } from 'mongodb';
 
 import { CollectionName } from '../../../mongodb/collection-names';
 
+import { createMongoDBLoaders } from '../../../mongodb/loaders';
+
 import { CollectionsStats } from './collection-stats';
 import { createMongoDBContext } from './context';
 
@@ -11,3 +13,13 @@ export const { mongoClient, mongoDB, mongoCollections, resetDatabase } =
 export const mongoCollectionStats = new CollectionsStats(
   mongoCollections as unknown as Record<CollectionName, Collection>
 );
+
+export function createMongoDBApiInstanceContext(collections = mongoCollections) {
+  return {
+    collections,
+    loaders: createMongoDBLoaders({
+      client: mongoClient,
+      collections: mongoCollections,
+    }),
+  };
+}
