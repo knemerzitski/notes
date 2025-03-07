05f80b8e499ed7c43972f6f4b2cfe9ab7477c86f
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-03-07 17:18:00 +0200

    refactor(api): rename mongodb to context

--- packages/api/src/services/user/insert-user-with-google-user.int.test.ts
+++ packages/api/src/services/user/insert-user-with-google-user.int.test.ts
@@ -1,11 +1,12 @@
 import { faker } from '@faker-js/faker';
 import { beforeEach, expect, it } from 'vitest';
 
+import { createMongoDBApiContext } from '../../__tests__/helpers/mongodb/context';
 import {
   mongoCollections,
   resetDatabase,
- mongoCollectionStats } from '../../__tests__/helpers/mongodb/instance';
-import { createMongoDBApiContext } from '../../__tests__/helpers/mongodb/mongodb';
+  mongoCollectionStats,
+} from '../../__tests__/helpers/mongodb/instance';
 
 import { insertUserWithGoogleUser } from './insert-user-with-google-user';
 
