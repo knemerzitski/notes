69037f98d367fdef8330c7e03e0164a82c92d184
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-03-16 21:22:04 +0200

    test(app): e2e test note creation

---
+++ packages/app/cypress/e2e/note-creation.cy.ts
@@ -0,0 +1,69 @@
+import { GraphQLService } from '../../src/graphql/types';
+
+let graphQLService: GraphQLService;
+
+beforeEach(() => {
+  cy.resetDatabase();
+
+  // Init GraphQLService
+  cy.graphQLService().then((value) => {
+    graphQLService = value.service;
+
+    // Sign in
+    cy.signIn({
+      graphQLService,
+      googleUserId: 'a',
+      displayName: 'FooUser',
+    });
+
+    cy.persistCache({
+      graphQLService,
+    });
+  });
+});
+
+function createNoteWidget() {
+  return cy.get('[aria-label="create note widget"]').filter(':visible');
+}
+
+function notesList() {
+  return cy.get(`[aria-label="notes list"]`);
+}
+
+function shouldHaveNotesCount(count: number) {
+  return notesList().find('> li').should('have.length', count);
+}
+
+function notesListItem(index: number, local = false) {
+  return notesList().find(`> li[data-is-local="${local}"]`).eq(index);
+}
+
+it('creates 1 note from widget', () => {
+  cy.visit('/');
+
+  createNoteWidget().click();
+  createNoteWidget().find('[aria-label="content"]').type('bar');
+  createNoteWidget().find('[aria-label="title"]').type('foo');
+  createNoteWidget().find('[aria-label="close"]').click();
+
+  notesListItem(0).should('include.text', 'foobar');
+  shouldHaveNotesCount(1);
+});
+
+it('creates 2 notes from widget', () => {
+  cy.visit('/');
+
+  createNoteWidget().click();
+  createNoteWidget().find('[aria-label="content"]').type('bar');
+  createNoteWidget().find('[aria-label="title"]').type('foo');
+  createNoteWidget().find('[aria-label="close"]').click();
+
+  createNoteWidget().click();
+  createNoteWidget().find('[aria-label="content"]').type('bar2');
+  createNoteWidget().find('[aria-label="title"]').type('foo2');
+  createNoteWidget().find('[aria-label="close"]').click();
+
+  notesListItem(0).should('include.text', 'foo2bar2');
+  notesListItem(1).should('include.text', 'foobar');
+  shouldHaveNotesCount(2);
+});
